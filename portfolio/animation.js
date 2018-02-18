9556a4fbf09384ced16ee80ff608f70e284e2f4e
mojoezone <mojoezone@gmail.com>
2018-02-17 19:25:34 -0800

    added rotate deg to animation

--- portfolio/animation.js
+++ portfolio/animation.js
@@ -1,21 +1,36 @@
+(function(){
 var animationJob = document.querySelector("#animationJob");
 var animationJobText = "A Frontend Web Developer and Designer";
 var textAnimation = animationJobText.split('');
+    
+var radiusWidth = window.innerWidth/2;
+var pi = 3.141592;
+var circumferencePix;
+//get the circumference
+var getCircumference =  Math.floor(circumferencePix = 2*pi*radiusWidth);
+var spaceText = Math.floor(getCircumference/animationJobText.length); 
+var getDeg = 360/(animationJobText.length);
+//const setDeg =()=> textAnimation[''].style.transform ='rotate('+getDeg+'deg)';
+
+
+
 
 function inputTextAnimation () {
     //only shown the last str if the on par because it finished before the settimeout, * the index for each str
            textAnimation.forEach(function(obj, index){
                setTimeout(function(){
-               animationJob.innerHTML += obj;
-                 }, 200*index);
+                   animationJob.innerHTML += obj;
+                   //animationJob.style.letterSpacing = spaceText + 'px';
+                   //index + 1 to set it equal to 360 deg because the index array is 0-35
+                   animationJob.style.transform = 'rotate('+getDeg*(index+1)+'deg)';  
+                 }, 150*index);  
               }); 
-
     }
 
 inputTextAnimation();
 setTimeout(function(){
 animationJob.addEventListener("mouseenter", function(){    
-        animationJob.innerHTML = " &#8595;Checkout My Resume, Click.&#8595;";
+        animationJob.innerHTML = " &#8595; Checkout My Resume &#8595;";
     animationJob.style.color = "#4A606E";
 }, false);
 
@@ -24,3 +39,9 @@ animationJob.addEventListener("mouseleave", function(){
     animationJob.style.color = "#000";
 }, false);
     }, 8000)
+    
+    
+    
+    
+    
+    }())
